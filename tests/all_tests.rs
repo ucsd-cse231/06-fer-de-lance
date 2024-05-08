b3b2f8d4671e415b54955fe982d66c90a7b06324
Ranjit Jhala <rjhala@ucsd.edu>
2024-05-08 15:56:35 -0700

    init fdl

--- tests/all_tests.rs
+++ tests/all_tests.rs
@@ -19,10 +19,45 @@ success_tests! {
         file: "even_odd.snek",
         input: "9",
         expected: "9\nfalse\nfalse",
-    }
+    },
+    {
+        name: lam0,
+        file: "lam0.snek",
+        input: "",
+        expected: "6",
+    },
+    {
+        name: lam1,
+        file: "lam1.snek",
+        input: "",
+        expected: "50",
+    },
+    {
+        name: lam2,
+        file: "lam1.snek",
+        input: "",
+        expected: "(vec 6 50)",
+    },
+    {
+        name: lam_fac,
+        file: "lam-fac.snek",
+        input: "5",
+        expected: "120",
+    },
+    {
+        name: lam_map,
+        file: "lam-map.snek",
+        input: "100",
+        expected: "(vec 110 (vec 120 (vec 130 false)))",
+    },
+
+
 }
 
-runtime_error_tests! {}
+runtime_error_tests! {
+    lam_arity: "arity mismatch",
+    lam_not_fun: "callee is not a function",
+}
 
 static_error_tests! {
     {
